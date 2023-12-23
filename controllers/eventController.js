4d7e9b572cec253f990aa34625673daf1e145383
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2023-12-23 21:03:26 +0700

    add event

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,5 +1,6 @@
 const db = require("../database/db");
 
+// GET EVENT -----------------------------------
 const getAllEvents = async (req, res) => {
   const result = await db.collection("event").get();
   if (result.empty) {
@@ -28,4 +29,40 @@ const getEventById = async (req, res) => {
   }
 };
 
-module.exports = { getAllEvents, getEventById };
+// ---------------------------------------------
+const createEvent = async (req, res) => {
+  const { name, longitude, latitude, type, picture, province, date } = req.body;
+  if (
+    !name ||
+    !longitude ||
+    !latitude ||
+    !type ||
+    !picture ||
+    !province ||
+    !date
+  ) {
+    return res.status(400).json({ message: "Please provide all field" });
+  }
+
+  const events = await db.collection("event").get();
+  const foundEvent = events.docs.find((event) => event.data().name === name);
+  if (foundEvent) {
+    return res.status(401).json({ message: "Event already exist" });
+  }
+
+  try {
+    const result = await db.collection("event").add({
+      name,
+      longitude,
+      latitude,
+      type,
+      picture,
+      province,
+      date,
+    });
+    res.status(201).json({ success: `New event ${name} created!` });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+module.exports = { getAllEvents, getEventById, createEvent };
--- routes/event.js
+++ routes/event.js
@@ -3,9 +3,11 @@ const rounter = express.Router();
 const {
   getAllEvents,
   getEventById,
+  createEvent,
 } = require("../controllers/eventController");
 
 rounter.get("/", getAllEvents);
 rounter.get("/id", getEventById);
+rounter.post("/create", createEvent);
 
 module.exports = rounter;
