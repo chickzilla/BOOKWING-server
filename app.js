9f4f874c36ceb6fe95af4a5bfaa5dfe4b623fe2d
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2023-12-23 18:34:51 +0700

    init project

---
+++ app.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
