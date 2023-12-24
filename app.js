08dab7c270e11d6553b533f6d47adff51ca1f751
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2023-12-24 19:26:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ app.use(express.json());
 const PORT = process.env.PORT || 8000;
 const cors = require("cors");
 app.use(cors());
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
 
 const db = require("./database/db");
 
@@ -19,11 +22,8 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+/*
 app.get("/testDB", async (req, res) => {
   const result = await db.collection("user").get();
   res.send(result.docs.map((doc) => doc.data()));
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
-});
+});*/
