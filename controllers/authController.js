669a6ebb9559e1768636851b4e247cadd23a83ca
sarunpat aiyimaphan <maxkubz@hotmail.com>
2023-12-27 22:51:25 +0700

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const db = require("../database/db");
+const {db} = require("../database/db");
 
 const LoginHandler = async (req, res) => {
   const { email, password } = req.body;
